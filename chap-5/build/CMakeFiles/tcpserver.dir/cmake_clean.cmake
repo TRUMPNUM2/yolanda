06202873515ab5bc061c1030c945e067872a2927
root <root@localhost.localdomain>
2020-06-16 12:02:51 +0800

    init c socket complile project

---
+++ chap-5/build/CMakeFiles/tcpserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/tcpserver.dir/tcp_server.o"
+  "tcpserver.pdb"
+  "tcpserver"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/tcpserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ chap-5/tcpclient.c
@@ -0,0 +1,56 @@
+#include "lib/common.h"
+
+#define MESSAGE_SIZE 102400
+
+void send_data(int sockfd)
+{
+    char *query;
+    query = malloc(MESSAGE_SIZE + 1);
+    int i = 0;
+    for (; i < MESSAGE_SIZE; i++)
+    {
+        query[i] = 'a';
+    }
+    query[MESSAGE_SIZE] = '\0';
+
+    const char *cp;
+    cp = query;
+    size_t remaining = strlen(query);
+    while (remaining)
+    {
+        int n_written = send(sockfd, cp, remaining, 0);
+        fprintf(stdout, "send into buffer %ld \n", n_written);
+        if (n_written <= 0)
+        {
+            error(1, errno, "send failed");
+            return;
+        }
+        remaining -= n_written;
+        cp += n_written;
+    }
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    // if (argc != 2)
+    //     error(1, 0, "usage: tcpclient <IPaddress>");
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(12345);
+    inet_pton(AF_INET, "172.0.0.1", &servaddr.sin_addr);
+    int connect_rt = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (connect_rt < 0)
+    {
+        error(1, errno, "connect failed ");
+    }
+    send_data(sockfd);
+    exit(0);
+}
